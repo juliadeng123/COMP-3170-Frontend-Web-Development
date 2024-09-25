1135396d564d3a8d3a4b26ea385b9a6a40e8d35a
juliadeng123 <jdeng43@my.bcit.ca>
2024-09-25 13:19:44 -0700

    add tiles and remove tile

--- labs/demo4/src/App.jsx
+++ labs/demo4/src/App.jsx
@@ -25,10 +25,45 @@ function App() {
     }
 
   return (
+    <>
       <div className='outer' onClick={handleOuterClick}>
         <div className='inner' onClick={handleInnerClick}></div>
       </div>
+
+      <div className='container'>
+        <Tile name ="Tile 1"/>
+        <Tile name ="Tile 2"/>
+        <Tile name ="Tile 3"/>
+        <Tile name ="Tile 4"/>
+        <Tile name ="Tile 5"/>
+        <Tile name ="Tile 6"/>
+      </div>
+    </>
   );
+}
+
+function Tile(props){
+    const background = `rgb(${random(255)}, ${random(255)}, ${random(255)})`
+
+    function handleDelete(e) {
+        if(e.target === e.currentTarget) {
+            // you clicked on the tile itself
+            return;
+        }
+        
+        if(e.target.tagName !== 'SPAN'){
+            return;
+        }
+
+        e.currentTarget.remove(); //otherwise, remove the tile
+    }
+
+    return(
+        <div className='tile' style={{ backgroundColor: background}} onClick={handleDelete}>
+            <p>{props.name}</p>
+            <span>x</span>
+        </div>
+    )
 }
 
 export default App;
