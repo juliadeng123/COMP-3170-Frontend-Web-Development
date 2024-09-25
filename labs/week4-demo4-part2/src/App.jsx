e66db0862a2491fedc350386bd151f2544ec57d9
juliadeng123 <jdeng43@my.bcit.ca>
2024-09-25 14:02:34 -0700

    add part 2 to demo 4

---
+++ labs/week4-demo4-part2/src/App.jsx
@@ -0,0 +1,14 @@
+import Editor from './components/Editor';
+import Output from './components/Output';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Simple text editor</h1>
+      <Editor />
+      <Output />
+    </div>
+  );
+}
+
+export default App;
