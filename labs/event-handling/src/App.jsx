1729e0285af3df90736d362ab76d8a3c8385bb2b
juliadeng123 <jdeng43@my.bcit.ca>
2024-09-25 12:18:30 -0700

    add code to event-handling

--- labs/event-handling/src/App.jsx
+++ labs/event-handling/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    // function greet(name) {
+    //     console.log(`Hello ${name}`);
+    // }
+
+    function handleClick(event) {
+        const btn = event.target;
+        btn.textContent = "React"
+    }
+
+    function handleLink(e) {
+        e.preventDefault(); //won't open the url in the browser
+        const element = e.target;
+        console.log(element.href);
+    }
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button onClick={handleClick}>Click me</button>
+        <a href="https://bcit.ca" target='_blank' onClick={handleLink}>Visit BCIT</a>
+        {/* <button onClick={() => greet("John Doe")}>Click Me!</button>
+        <button onClick={() => greet("Jane Doe")}>Click Me!</button>
+        <button onClick={() => greet("Chad")}>Click Me!</button> */}
     </>
   )
 }
