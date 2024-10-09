143e876fa56ead68ed6788c0e742ae2cd09a943b
juliadeng123 <jdeng43@my.bcit.ca>
2024-10-09 11:45:59 -0700

    add week 6 examples

---
+++ labs/week6-examples-states/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+
+    let name = "John Doe";
+
+    return (
+        <>
+     
+        </>
+  )
+}
+
+export default App
