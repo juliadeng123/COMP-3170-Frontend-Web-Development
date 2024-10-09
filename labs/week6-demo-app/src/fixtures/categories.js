94e3a405e85125e1ea2be62acd94a17255e790bb
juliadeng123 <jdeng43@my.bcit.ca>
2024-10-09 12:52:04 -0700

    add base files for week 6 demo app

---
+++ labs/week6-demo-app/src/fixtures/categories.js
@@ -0,0 +1,18 @@
+export const categories = [
+    {
+      id: 0,
+      name: "uncategorized"
+    },
+    {
+      id: 1,
+      name: "clothing"
+    },
+    {
+      id: 2,
+      name: 'footwear'
+    },
+    {
+      id: 3,
+      name: 'hats'
+    }
+];
